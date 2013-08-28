2ad65a2b455fe27f5a120bc14d7fdb9f115cf447
Serqueg10 <sergiogr9506@gmail.com>
2013-08-28 10:06:03 -0500

    first commit

---
+++ pruebalmb/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pruebalmb::Application.config.secret_key_base = 'c5507866a9330652f89be6c11534ba8a5ed94b02922bb1b61dd5e3cc67424e16baba89a5bf4776cb9c7c45c85a541b6b082b8226cebedd36632c34c071325069'
---
+++ railsinstaller_demo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsinstallerDemo::Application.config.secret_key_base = '3033fad861b11ddce8c4b3b2eb131e87acafb16a2636d67978d9c2559a1b5c51d00987976624668d0ff61a935a3d6a158420a873a9cc3c7e37591a8c6bc33f82'
